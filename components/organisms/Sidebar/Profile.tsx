8cdf8186285025cb3f8b87b3143c8bbe6194f938
dharmawiguna <dharmawiguna982@gmail.com>
2023-09-14 13:23:25 +0800

    sidebar integration

--- components/organisms/Sidebar/Profile.tsx
+++ components/organisms/Sidebar/Profile.tsx
@@ -1,14 +1,39 @@
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import { JwtPayloadTypes, UserTypes } from "../../../services/data-types";
+import jwtDecode from "jwt-decode";
+
 export default function Profile() {
+  const [user, setUser] = useState({
+    avatar: "",
+    name: "",
+    email: "",
+  });
+
+  const img = process.env.NEXT_PUBLIC_IMAGE;
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JwtPayloadTypes = jwtDecode(jwtToken);
+      const userPayload: UserTypes = payload.player;
+      setUser(userPayload);
+    }
+  }, []);
   return (
     <div className="user text-center pb-50 pe-30">
       <img
-        src="/img/avatar-1.png"
+        src={`${img}/${user.avatar}`}
         width="90"
         height="90"
-        className="img-fluid mb-20"
+        className="img-fluid mb-20 rounded-circle"
+        style={{ borderRadius: "100%" }}
       />
-      <h2 className="fw-bold text-xl color-palette-1 m-0">Shayna Anne</h2>
-      <p className="color-palette-2 m-0">shayna@anne.com</p>
+      <h2 className="fw-bold text-xl color-palette-1 m-0">{user.name}</h2>
+      <p className="color-palette-2 m-0" style={{ fontSize: "13px" }}>
+        {user.email}
+      </p>
     </div>
   );
 }
